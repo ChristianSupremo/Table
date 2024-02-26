1af9094491721e7eb43773f4b9e85647cd017866
ChristianSupremo <159906422+ChristianSupremo@users.noreply.github.com>
2024-02-26 20:03:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var originalData = [
+    ["1", "Juan", "Dela Cruz", "BS IT"],
+    ["2", "Maria", "Cruz", "BS CS"],
+    ["3", "Captain", "America", "BS CS"]
+];
+
+function populateTable() {
+    var tbody = document.getElementById("studentTable").getElementsByTagName("tbody")[0];
+    originalData.forEach(function(rowData) {
+        var row = document.createElement("tr");
+        rowData.forEach(function(cellData) {
+            var cell = document.createElement("td");
+            cell.textContent = cellData;
+            cell.className = "editable";
+            cell.onclick = function() { editCell(this); };
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+}
+
+function editCell(cell) {
+    var currentValue = cell.innerText;
+    cell.innerHTML = `<input type="text" value="${currentValue}" onblur="saveCell(this)">`;
+    cell.querySelector("input").focus();
+}
+
+function saveCell(input) {
+    var newValue = input.value;
+    input.parentNode.innerText = newValue;
+}
+
+function resetTable() {
+    var tbody = document.getElementById("studentTable").getElementsByTagName("tbody")[0];
+    tbody.innerHTML = "";
+    populateTable();
+}
+
+populateTable();
+
+document.getElementById("loadListButton").addEventListener("click", resetTable);
